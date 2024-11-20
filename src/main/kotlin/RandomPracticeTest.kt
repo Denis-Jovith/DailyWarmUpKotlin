f5cc89c7b63523630a099939308d410ee1affb95
Denis-Jovith <denisjovitusbuberwa@gmail.com>
2024-11-20 08:44:00 +0300

    used the primary constructor

--- src/main/kotlin/RandomPracticeTest.kt
+++ src/main/kotlin/RandomPracticeTest.kt
@@ -1,19 +1,31 @@
-class Bidhaa{
-    var jina = "iphone 14"
-        set(value){
-            field = value.uppercase()
-        }
-     var bei = "500000"
-        get(){
-            return "Tsh. $field /="
-        }
-}
-fun showProductInfo(){
+//class Bidhaa(val ){
+//    var jina = "iphone 14"
+//        set(value){
+//            field = value.uppercase()
+//        }
+//     var bei = "500000"
+//        get(){
+//            return "Tsh. $field /="
+//        }
+//}
 
+class Bidhaa(var jina: String, var bei: Int) {
+    // Function to display product information
+    fun showProductInfo() {
+        println("Jina: $jina, Bei: Tsh. $bei /=")
+    }
 }
 
-fun main(){
-    var b1 = Bidhaa()
-     b1.jina = "iphone X"
+fun main() {
+    // Creating an instance of the Bidhaa class
+    var b1 = Bidhaa(bei = 500, jina = "Sabuni")
+
+    // Changing the 'jina' property
+    b1.jina = "iphone X"
+
+    // Displaying the product information
+    b1.showProductInfo() // This will call the function within the class
+
+    // Printing just the name
     println(b1.jina)
 }
