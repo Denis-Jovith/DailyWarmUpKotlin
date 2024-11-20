98676069c9a9b823c21e140fc2b557e1b1e2b680
Denis-Jovith <denisjovitusbuberwa@gmail.com>
2024-11-20 15:48:24 +0300

    Used a function as a return type

--- src/main/kotlin/AnotherPlaygroun.kt
+++ src/main/kotlin/AnotherPlaygroun.kt
@@ -1,16 +1,42 @@
-fun main() {
-    val trickFunction = ::trick
+//fun main() {
+//    val trickFunction = ::trick
+//    trickFunction()
+//
+//    val treat: () -> Unit = {
+//        println("Have a treat!")
+//    }
+//    treat()
+//
+//
+//}
+//
+//fun trick() {
+//    println("No treats!")
+//}
+//
+//
+
+fun main(){
+    val treatFunction = trickOrTreat(false)
+    val trickFunction = trickOrTreat(true)
+
+    treatFunction()
     trickFunction()
-
-    val treat: () -> Unit = {
-        println("Have a treat!")
-    }
-    treat()
-
-
 }
 
-fun trick() {
+fun trickOrTreat(isTrick:Boolean): () -> Unit {
+    if(isTrick){
+        return trick
+    }
+    else{
+        return treat
+    }
+}
+
+val trick = {
     println("No treats!")
 }
 
+val treat = {
+    println("Have a treat!")
+}
