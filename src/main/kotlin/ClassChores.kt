122b704d3409af0d6bdd3716acc912cc69d72780
Denis-Jovith <denisjovitusbuberwa@gmail.com>
2024-11-21 17:51:25 +0300

    I created a simple interest calcurator

--- src/main/kotlin/ClassChores.kt
+++ src/main/kotlin/ClassChores.kt
@@ -1,15 +1,36 @@
-open class Animali(val name:String){
-    fun move(){
+//open class Animali(val name:String){
+//    protected fun move(){
+//        println("Animal is moving")
+//    }
+//
+//    fun anotherMove (){
+//        move()
+//    }
+//
+//
+//
+//}
+//
+//class Snakei(name: String):Animali(name){
+//
+//}
+//
 
+class Rate(val p:Int,val r:Int,val y:Int){
+    //p*r*t/100
+    fun multiply():Int{
+        return p*r*y
+    }
+
+    fun showRate(){
+        println("The rate is ${multiply()/100}")
     }
 }
 
-class Snakei(name: String):Animali("Anakonda"){
-
-}
-
-
 fun main(){
-    val nyoka = Snake()
+//    val anakonda = Snakei("Anakonda")
+//    anakonda.anotherMove()
 
+    var r = Rate(5000,3,5)
+    r.showRate()
 }
