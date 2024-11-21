c18443473ec62246fbaac5d07a10db3c5e96cd45
Denis-Jovith <denisjovitusbuberwa@gmail.com>
2024-11-21 18:05:54 +0300

    USED SETTER AND GETTER

--- src/main/kotlin/delegates.kt
+++ src/main/kotlin/delegates.kt
@@ -1,9 +1,17 @@
 class Speaker{
     var speakerVolume : Int = 0
         set(value){
+            if(value in 0..60)
             field = value
+        }
+        get(){
+            return field
         }
 }
 fun main(){
-
+    val seaPeano = Speaker()
+    seaPeano.speakerVolume = 20
+    println("The volume level is : ${seaPeano.speakerVolume}")
+    seaPeano.speakerVolume = 80
+    println("The volume level is : ${seaPeano.speakerVolume}")
 }
