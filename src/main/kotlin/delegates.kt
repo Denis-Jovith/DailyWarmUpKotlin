e48a6deabd3089b2a6f68715612a2f86bfb6248f
Denis-Jovith <denisjovitusbuberwa@gmail.com>
2024-11-22 19:40:35 +0300

    Dealing with delegates

--- src/main/kotlin/delegates.kt
+++ src/main/kotlin/delegates.kt
@@ -1,17 +1,47 @@
-class Speaker{
-    var speakerVolume : Int = 0
-        set(value){
-            if(value in 0..60)
-            field = value
-        }
-        get(){
-            return field
-        }
-}
-fun main(){
-    val seaPeano = Speaker()
-    seaPeano.speakerVolume = 20
-    println("The volume level is : ${seaPeano.speakerVolume}")
-    seaPeano.speakerVolume = 80
-    println("The volume level is : ${seaPeano.speakerVolume}")
-}
+//import kotlin.properties.ReadWriteProperty
+//import kotlin.reflect.KProperty
+//
+//class VolumeRegulator (
+//    initialValue:Int,
+//    minValue:Int,
+//    maxValue:Int
+//
+//):ReadWriteProperty<Any?, String> {
+//    var fieldData = initialValue
+//    override fun setValue(thisRef: Any?, property: KProperty<*>, value: Int) {
+//        if(value in 0..60)
+//            fieldData = value
+//    }
+//     override fun getValue(thisRef: Any?,value: Int){
+//        return fieldData
+//    }
+//}
+//
+//class Speaker{
+//    var speakerVolume : Int by VolumeRegulator(7,0,0)
+//
+//    var centerVolume : Int = 0
+//        set(value){
+//            if(value in 0..60)
+//                field = value
+//        }
+//        get(){
+//            return field
+//        }
+//
+//    var frontVolume : Int = 0
+//        set(value){
+//            if(value in 0..60)
+//                field = value
+//        }
+//        get(){
+//            return field
+//        }
+//
+//    var sorroundVolume : Int
+//
+//}
+//fun main(){
+//    val seaPeano = Speaker()
+//
+//}
