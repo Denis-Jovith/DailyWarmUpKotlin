ea7c5ddf0a91ed2fe6f99b2054192eff7a6a4591
Denis-Jovith <denisjovitusbuberwa@gmail.com>
2024-11-22 19:42:50 +0300

    Learned to return fungction

---
+++ src/main/kotlin/returnFunction.kt
@@ -0,0 +1,28 @@
+//fun main(){
+//    val s = salamu("mchana")
+//    val s = salamu({ println("mambo veep") }.toString();)
+//
+//    s()
+//}
+//
+// public val salamu: (s:String)->()-> Unit = {
+//    majira->
+//    if(majira=="asubuhi")
+//        return salamuYaAsubuhi
+//    else if(majira == "mchana")
+//        return salamuYamchana
+//    else
+//        return salamuYaJioni
+//}
+//
+//val salamuYaAsubuhi: ()-> Unit ={
+//    println("Habari za asubuhi")
+//}
+//
+//val salamuYamchana= {
+//    println("Habari za mchana")
+//}
+//
+//val salamuYaJioni = {
+//    println("Habari za jioni")
+//}
