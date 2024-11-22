45f240abfc9b8e168549ada25d0b24bd3547c5ba
Denis-Jovith <denisjovitusbuberwa@gmail.com>
2024-11-22 13:05:19 +0300

    Just codin

---
+++ src/main/kotlin/TodaysWarmUp.kt
@@ -0,0 +1,11 @@
+fun main(){
+firstFunction("Denis")
+    var height=21
+    firstFunction("Denis")
+    println(height)
+}
+
+fun firstFunction(name:String){
+    var age=21
+    println("Hello $name you are $age years old")
+}
