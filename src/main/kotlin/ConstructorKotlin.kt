3f3083ca75849e45ef855ce39d5026cc50243e01
Denis-Jovith <denisjovitusbuberwa@gmail.com>
2024-11-28 16:17:39 +0300

    I added the secondary constructor

--- src/main/kotlin/ConstructorKotlin.kt
+++ src/main/kotlin/ConstructorKotlin.kt
@@ -1,20 +1,21 @@
+class SmartTvDevicee(var name:String,val category :String){
+    var deviceStatus = "online"
+
+    constructor(name:String,category: String,statusCode:Int) :
+    this (name,category)
+    {
+        deviceStatus = when(statusCode){
+            0->"offline"
+            1->"online"
+            else -> {
+                "unknown"
+            }
+        }
+    }
+
+}
+
 fun main(){
-
-}
-
-open class SmartDevicee(val name:String, val category:String){
-var deviceStatus = "online"
-}
-
-fun turnOn(){
-    println("Smart device is turned on")
-}
-
-fun turnOff(){
-    println("Smart device is turned off.")
-}
-
-class SmartTviDevice(deviceName:String,deviceCategory:String):
-SmartDevicee(name = deviceName, category = deviceCategory){
-
+    val smartTv = SmartDevice("Sony","Flat Screen")
+    println("The name is : ${smartTv.name}  and \nThe categry is ${smartTv.category}")
 }
