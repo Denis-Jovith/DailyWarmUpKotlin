25436ab7ebf746ef4c7ac1fccd435b562da61b01
Denis-Jovith <denisjovitusbuberwa@gmail.com>
2024-11-30 12:58:31 +0300

    tried to create mobile notification application

--- src/main/kotlin/MobileNotification.kt
+++ src/main/kotlin/MobileNotification.kt
@@ -1,2 +1,14 @@
-class MobileNotification {
+public fun main(){
+    val morningNotification = 51
+    val eveningNotification = 135
+    val numberOfMessages = morningNotification+eveningNotification
+
+    val majina = arrayListOf("how","jd")
+
+
+//    println(printNotificationSummary(numberOfMessages))
+}
+
+fun printNotificationSummary(numberOfMessages:Int,morningNotification:Int): String {
+    return "You have  51 notification\n"+ "Your phone is blowing up! You have 99+ notification"
 }
