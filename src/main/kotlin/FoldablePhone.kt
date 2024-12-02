f6f3d615ac53dfe8e9200bdfc79e0a1fb8404da5
Denis-Jovith <denisjovitusbuberwa@gmail.com>
2024-12-02 11:03:43 +0300

    Applied  the knowledge of inheritance to create a foldable phone program

---
+++ src/main/kotlin/FoldablePhone.kt
@@ -0,0 +1,48 @@
+fun main(){
+    val foldablePhone = FoldablePhone()
+    foldablePhone.checkPhoneScreenLight()
+
+    foldablePhone.unfoldPhone()
+    foldablePhone.switchOn()
+
+    foldablePhone.foldPhone()
+    foldablePhone.switchOn()
+
+    foldablePhone.checkPhoneScreenLight()
+}
+
+open class Phone(var isScreenLightOn:Boolean = false){
+    open fun switchOn(){
+        isScreenLightOn = true
+    }
+
+    fun switchOff(){
+        isScreenLightOn = false
+    }
+
+    fun checkPhoneScreenLight(){
+        val phoneScreenLight = if (isScreenLightOn) "on" else "off"
+        println("The phone screen's light is $phoneScreenLight.")
+    }
+}
+
+class FoldablePhone(isScreenLightOn: Boolean = false,var isFolded:Boolean=false):Phone(isScreenLightOn){
+    override fun switchOn(){
+        if(isFolded){
+            println("The phone is folded.The screen cannot be turned on.")
+        }else{
+            super.switchOn()
+            println("The phone screen is now on.")
+        }
+    }
+
+    fun foldPhone(){
+        isFolded = true
+        println("The phone is now folded.")
+    }
+
+    fun unfoldPhone(){
+        isFolded = false
+        println("The phone is now unfolded.")
+    }
+}
