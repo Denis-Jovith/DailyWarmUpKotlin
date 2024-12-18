0e9bf840ce6a5d7d6d46607dfcf7322f4c96d035
Denis-Jovith <denisjovitusbuberwa@gmail.com>
2024-12-18 20:58:55 +0300

    Learned how to use Generic in Class

---
+++ src/main/kotlin/Reusable Class With Generic.kt
@@ -0,0 +1,15 @@
+class Question<T>(
+    val questionText:String,
+    val answer : T,
+    val difficulty:String
+)
+
+fun main(){
+    val question1 = Question<String>("Quoth the raven ___","nevermore","medium")
+    val question2 = Question<Boolean>("The sky is green. True or False",false,"easy")
+    val question3 = Question<Int>("How many days are there between full moons?",28,"hard")
+
+    println(question1.questionText)
+    println(question1.answer)
+    println(question1.difficulty)
+}
